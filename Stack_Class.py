6ad06dc00df8c7364b06423661a4e482c4d68e34
Kavi Pandya <pandyakavi@rocketmail.com>
2017-05-22 13:03:43 +0530

    Self-written Stack Data Structure

---
+++ Stack_Class.py
@@ -0,0 +1,27 @@
+# Peek, Pop, isEmpty, Push, size()
+
+class Stack_Cls:
+
+	def __init__(self):
+		self.items = []
+
+	def Peek(self):
+		return self.items[-1]
+
+	def Pop(self):
+		#k = self.items[-1]
+		#self.items.remove(k)
+		return self.items.pop()#k
+
+	def isEmpty(self):
+		return self.items == []
+
+	def Push(self,val):
+		self.items.append(val)
+
+	def size(self):
+		return len(self.items)
+
+	def Print(self):
+		print(self.items)
+		
